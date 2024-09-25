87d167d31e71b52298d022c6c4da8e7b26e34bc6
monkeyK1n9 <mkeyk1n9@gmail.com>
2024-09-25 11:46:55 +0200

    create fake games

--- GameStore.Frontend/Models/GameSummary.cs
+++ GameStore.Frontend/Models/GameSummary.cs
@@ -4,5 +4,9 @@ namespace GameStore.Frontend.Models;
 
 public class GameSummary
 {
-
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public DateOnly ReleaseDate { get; set; }
 }
