272115ed8e67a5b18390cb5e16ca12b3cabc542c
monkeyK1n9 <mkeyk1n9@gmail.com>
2024-12-15 01:48:18 +0100

    Update: move genres client and affected files to async

--- GameStore.Frontend/Clients/GenresClient.cs
+++ GameStore.Frontend/Clients/GenresClient.cs
@@ -5,29 +5,5 @@ namespace GameStore.Frontend.Clients;
 
 public class GenresClient(HttpClient httpClient)
 {
-    private readonly List<Genre> _genres =
-    [
-        new() {
-            Id = 1,
-            Name = "Fighting"
-        },
-        new() {
-            Id = 2,
-            Name = "Roleplaying"
-        },
-        new() {
-            Id = 3,
-            Name = "Sports"
-        },
-        new() {
-            Id = 4,
-            Name = "Racing"
-        },
-        new() {
-            Id = 5,
-            Name = "Kids and Family"
-        }
-    ];
-
-    public Genre[] GetGenres() => [.. _genres];
+    public async Task<Genre[]> GetGenresAsync() => await httpClient.GetFromJsonAsync<Genre[]>("genres") ?? [];
 }
